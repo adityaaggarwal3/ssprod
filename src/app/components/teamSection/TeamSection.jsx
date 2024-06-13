4a235dad818b76c8ee0d04d00a2d66036447351d
Aditya <aditya.aggarwal@live.com>
2024-06-13 20:45:15 +0530

    Team Section updated

--- src/app/components/teamSection/TeamSection.jsx
+++ src/app/components/teamSection/TeamSection.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import { FaLinkedinIn, FaFacebookF, FaInstagram } from "react-icons/fa";
+import { motion } from "framer-motion";
 import styles from "./teamsection.module.css";
 
 const TeamSection = () => {
@@ -21,7 +22,13 @@ const TeamSection = () => {
     <div className={styles.teamSection}>
       <h2>Our Team</h2>
       <div className={styles.cardContainer}>
-        <div className={styles.singleCard}>
+        <motion.div
+          className={styles.singleCard}
+          initial={{ y: 250, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ delay: 0.75, duration: 0.5 }}
+          viewport={{ once: true }}
+        >
           <div className={styles.memberImgContainer}>
             <Image
               className={styles.memberImg}
@@ -39,8 +46,14 @@ const TeamSection = () => {
               <FaInstagram size={30} color="#fa7c07" />
             </div>
           </div>
-        </div>
-        <div className={styles.singleCard}>
+        </motion.div>
+        <motion.div
+          className={styles.singleCard}
+          initial={{ y: 250, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ delay: 0.75, duration: 0.5 }}
+          viewport={{ once: true }}
+        >
           <div className={styles.memberImgContainer}>
             <Image
               className={styles.memberImg}
@@ -58,8 +71,14 @@ const TeamSection = () => {
               <FaInstagram size={30} color="#fa7c07" />
             </div>
           </div>
-        </div>
-        <div className={styles.singleCard}>
+        </motion.div>
+        <motion.div
+          className={styles.singleCard}
+          initial={{ y: 250, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ delay: 0.75, duration: 0.5 }}
+          viewport={{ once: true }}
+        >
           <div className={styles.memberImgContainer}>
             <Image
               className={styles.memberImg}
@@ -77,8 +96,14 @@ const TeamSection = () => {
               <FaInstagram size={30} color="#fa7c07" />
             </div>
           </div>
-        </div>
-        <div className={styles.singleCard}>
+        </motion.div>
+        <motion.div
+          className={styles.singleCard}
+          initial={{ y: 250, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ delay: 0.75, duration: 0.5 }}
+          viewport={{ once: true }}
+        >
           <div className={styles.memberImgContainer}>
             <Image
               className={styles.memberImg}
@@ -96,8 +121,14 @@ const TeamSection = () => {
               <FaInstagram size={30} color="#fa7c07" />
             </div>
           </div>
-        </div>
-        <div className={styles.singleCard}>
+        </motion.div>
+        <motion.div
+          className={styles.singleCard}
+          initial={{ y: 250, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ delay: 0.75, duration: 0.5 }}
+          viewport={{ once: true }}
+        >
           <div className={styles.memberImgContainer}>
             <Image
               className={styles.memberImg}
@@ -115,8 +146,14 @@ const TeamSection = () => {
               <FaInstagram size={30} color="#fa7c07" />
             </div>
           </div>
-        </div>
-        <div className={styles.singleCard}>
+        </motion.div>
+        <motion.div
+          className={styles.singleCard}
+          initial={{ y: 250, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ delay: 0.75, duration: 0.5 }}
+          viewport={{ once: true }}
+        >
           <div className={styles.memberImgContainer}>
             <Image
               className={styles.memberImg}
@@ -134,8 +171,14 @@ const TeamSection = () => {
               <FaInstagram size={30} color="#fa7c07" />
             </div>
           </div>
-        </div>
-        <div className={styles.singleCard}>
+        </motion.div>
+        <motion.div
+          className={styles.singleCard}
+          initial={{ y: 250, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ delay: 0.75, duration: 0.5 }}
+          viewport={{ once: true }}
+        >
           <div className={styles.memberImgContainer}>
             <Image
               className={styles.memberImg}
@@ -153,45 +196,7 @@ const TeamSection = () => {
               <FaInstagram size={30} color="#fa7c07" />
             </div>
           </div>
-        </div>
-        <div className={styles.singleCard}>
-          <div className={styles.memberImgContainer}>
-            <Image
-              className={styles.memberImg}
-              src="https://images.unsplash.com/photo-1540569014015-19a7be504e3a?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              width={150}
-              height={150}
-            />
-          </div>
-          <div className={styles.memberInfo}>
-            <h3 className={styles.memberName}>John Doe</h3>
-            <h3 className={styles.memberDesignation}>Designation</h3>
-            <div className={styles.socialIcons}>
-              <FaLinkedinIn size={30} color="#fa7c07" />
-              <FaFacebookF size={30} color="#fa7c07" />
-              <FaInstagram size={30} color="#fa7c07" />
-            </div>
-          </div>
-        </div>
-        <div className={styles.singleCard}>
-          <div className={styles.memberImgContainer}>
-            <Image
-              className={styles.memberImg}
-              src="https://images.unsplash.com/photo-1540569014015-19a7be504e3a?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              width={150}
-              height={150}
-            />
-          </div>
-          <div className={styles.memberInfo}>
-            <h3 className={styles.memberName}>John Doe</h3>
-            <h3 className={styles.memberDesignation}>Designation</h3>
-            <div className={styles.socialIcons}>
-              <FaLinkedinIn size={30} color="#fa7c07" />
-              <FaFacebookF size={30} color="#fa7c07" />
-              <FaInstagram size={30} color="#fa7c07" />
-            </div>
-          </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
