21cf6ae7f7e9f766037d218521b3e12969204b2c
Aditya <aditya.aggarwal@live.com>
2024-06-14 06:49:36 +0530

    service page wireframe added

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -31,7 +31,41 @@ const AboutPage = () => {
         transition={{ delay: 0.5, duration: 0.7 }}
         viewport={{ once: true }}
       >
-        <Image src="/logopreview.png" width={500} height={300} />
+        <Image src="/logopreview.png" width={500} height={280} />
+      </motion.div>
+      <motion.div
+        className={styles.aboutcontainer}
+        initial={{ y: 250, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.5, duration: 0.7 }}
+        viewport={{ once: true }}
+      >
+        <div className={styles.aboutText}>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse
+            corrupti delectus, neque autem pariatur reiciendis deleniti
+            laboriosam vel rem aperiam ipsa, rerum aliquid a. Optio, eligendi
+            ut. Tempora, eaque exercitationem! Hic labore officiis nulla
+            praesentium aliquid repellendus aut, eaque voluptatibus fugit,
+            maiores autem, expedita inventore ad et amet laboriosam
+            reprehenderit minima consectetur. Repellat voluptas assumenda veniam
+            iure dolorem consectetur sed. Lorem, ipsum dolor sit amet
+            consectetur adipisicing elit. Esse corrupti delectus, neque autem
+            pariatur reiciendis deleniti laboriosam vel rem aperiam ipsa, rerum
+            aliquid a. Optio, eligendi ut. Tempora, eaque exercitationem! Hic
+            labore officiis nulla praesentium aliquid repellendus aut, eaque
+            voluptatibus fugit, maiores autem, expedita inventore ad et amet
+            laboriosam reprehenderit minima consectetur. Repellat voluptas
+            assumenda veniam iure dolorem consectetur sed.
+          </p>
+        </div>
+        <div className={styles.aboutImg}>
+          <Image
+            src="https://images.pexels.com/photos/236698/pexels-photo-236698.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            width={500}
+            height={300}
+          />
+        </div>
       </motion.div>
     </motion.div>
   );
--- src/app/about/services/page.jsx
+++ src/app/about/services/page.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import styles from "./services.module.css";
 
 const ServicesPage = () => {
-  return <div>ServicesPage</div>;
+  return <div className={styles.servicePage}>ServicesPage</div>;
 };
 
 export default ServicesPage;
